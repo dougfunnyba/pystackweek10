a20a83a8bbb51adf3e80015393ccdba4ce6dcd78
Douglas Silva <douglassilva@MacBook-Air-de-Douglas.local>
2024-04-19 17:02:43 -0300

    Iniciando aula 2

--- healing/settings.py
+++ healing/settings.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import os
 
+from django.contrib.messages import constants
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'usuarios',
+    'medico',
 ]
 
 MIDDLEWARE = [
@@ -117,3 +120,12 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configurando mensagens
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+    constants.WARNING: 'alert-warning',
+}
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,69 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
+from django.contrib import auth
 
 def cadastro(request):
     if request.method == "GET":
         return render(request, 'cadastro.html')
+    
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        
+        users = User.objects.filter(username=username)
+        
+        if users.exists():
+            messages.add_message(request, constants.ERROR, 'Este usuário já esta cadastrado no sistema.')
+            return redirect('/usuarios/cadastro')
+        
+        if senha != confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'As senhas não conferem.')
+            return redirect('/usuarios/cadastro')
+        
+        if len(senha) < 6:
+            messages.add_message(request, constants.ERROR, 'A senha tem que possuir pelo menos 6 caracteres.')
+            return redirect('/usuarios/cadastro')
+        
+        try:
+            user = User.objects.create_user(
+                username=username,
+                email=email,
+                password=senha
+            )
+            
+            messages.add_message(request, constants.SUCCESS, 'Usuário criado com sucesso.')
+            return redirect('/usuarios/login')
+        
+        except:
+            messages.add_message(request, constants.ERROR, 
+                                 'Erro inesperado do sistema, entre em contato com o administrador do sistema.')
+            return redirect('/usuarios/cadastro')
+        
+def login_view(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('senha')
+        
+        if not username or not password:
+            messages.add_message(request, constants.ERROR, 'Informe usuário e senha.')
+            return redirect('/usuarios/login')
+        
+        user = auth.authenticate(request, username=username, password=password)
+        
+        if user:
+            auth.login(user)
+            return redirect('/pacientes/home')
+        
+        messages.add_message(request, constants.ERROR, 'Usuário ou senha inválidos.')
+        return redirect('/usuarios/login')
+    
+def logout_view(request):
+    auth.logout(request)
+    return redirect('usuarios/login')
